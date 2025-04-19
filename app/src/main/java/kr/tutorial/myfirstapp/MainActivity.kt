7a3646606a91321850bb5d1f635eecab94d4e2a1
SEUNGREEJEON <jws08219@naver.com>
2025-04-19 12:35:05 +0900

    MainActivity.kt 설명

--- app/src/main/java/kr/tutorial/myfirstapp/MainActivity.kt
+++ app/src/main/java/kr/tutorial/myfirstapp/MainActivity.kt
@@ -13,12 +13,16 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import kr.tutorial.myfirstapp.ui.theme.MyFirstAppTheme
 
+// 액티비티란 모바일기기의 화면을 의미하며 화면간의 이동을 액티비티에서 하나의 액티비티로 이동한다고 말한다.
+// 아래 MainActivity뒤에 하나의 클래스가 붙는데 메인 액티비티를 ComponentActivity로 동작시킨다는 의미이다.
+// Java의 extends ComponentActivity라고 볼수있다. new Instance는 아닐수밖에 없는게 오바라이드와 상속자의 생성자 호출인 super가 존재하기때문이다.
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             MyFirstAppTheme {
+                // modifier는 코틀린타입중 하나
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
@@ -38,10 +42,11 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
+// AVD(Android Virtual Device) 가상시뮬까지 구동시킬 필요없이 코드를 프리뷰로 볼수있는 기능
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MyFirstAppTheme {
-        Greeting("Android")
+        Greeting("HAHA")
     }
 }
