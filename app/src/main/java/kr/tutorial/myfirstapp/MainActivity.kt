876c2889485ae885691d09fbc73ead6b73b95bf7
SEUNGREEJEON <jws08219@naver.com>
2025-04-21 10:39:18 +0900

    단위변환기앱_1만들기

--- app/src/main/java/kr/tutorial/myfirstapp/MainActivity.kt
+++ app/src/main/java/kr/tutorial/myfirstapp/MainActivity.kt
@@ -1,16 +1,35 @@
 package kr.tutorial.myfirstapp
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import kr.tutorial.myfirstapp.ui.theme.MyFirstAppTheme
 
 // 액티비티란 모바일기기의 화면을 의미하며 화면간의 이동을 액티비티에서 하나의 액티비티로 이동한다고 말한다.
@@ -23,11 +42,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyFirstAppTheme {
                 // modifier는 코틀린타입중 하나
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface {
+                    UnitConverter()
                 }
             }
         }
@@ -42,11 +58,116 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-// AVD(Android Virtual Device) 가상시뮬까지 구동시킬 필요없이 코드를 프리뷰로 볼수있는 기능
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    MyFirstAppTheme {
-        Greeting("HAHA")
+fun UnitConverter() {
+
+    Column (
+        // 컬럼이 전체 영역 차지
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text("Unit Converter", modifier = Modifier.padding(16.dp))
+        Spacer(modifier = Modifier.height(8.dp))
+        OutlinedTextField(value = "", onValueChange = {})
+        Row {
+            // 드롭다운 메뉴는 위치를 잡아주는 부모가 필요하기에 Box로 각 드롭다운버튼을 감싸준다
+            Box {
+                // 버튼 클릭시 토스트 팝업으로 Thanks for clicking이 뜬다.
+                Button(onClick = {}) {
+                    // 버튼 메시지
+                    Text("Select")
+                    Icon(Icons.Default.ArrowDropDown, contentDescription = "")
+                }
+                DropdownMenu(expanded = false, onDismissRequest = {}) {
+                    DropdownMenuItem(
+                        text = { Text("Centimeters")},
+                        onClick = {}
+                    )
+                    DropdownMenuItem(
+                        text = { Text("Meters")},
+                        onClick = {}
+                    )
+                    DropdownMenuItem(
+                        text = { Text("Feet")},
+                        onClick = {}
+                    )
+                    DropdownMenuItem(
+                        text = { Text("Millimeters")},
+                        onClick = {}
+                    )
+                }
+            }
+            Spacer(modifier = Modifier.width(16.dp))
+            Box {
+                // 버튼 클릭시 토스트 팝업으로 Thanks for clicking이 뜬다.
+                Button(onClick = {}) {
+                    // 버튼 메시지
+                    Text("Select")
+                    Icon(Icons.Default.ArrowDropDown, contentDescription = "")
+                }
+                DropdownMenu(expanded = false, onDismissRequest = {}) {
+                    DropdownMenuItem(
+                        text = { Text("Centimeters")},
+                        onClick = {}
+                    )
+                    DropdownMenuItem(
+                        text = { Text("Meters")},
+                        onClick = {}
+                    )
+                    DropdownMenuItem(
+                        text = { Text("Feet")},
+                        onClick = {}
+                    )
+                    DropdownMenuItem(
+                        text = { Text("Millimeters")},
+                        onClick = {}
+                    )
+                }
+            }
+        }
+        Text("Result : ")
     }
 }
+
+// 버튼, 토스트 관련 설명
+//@Composable
+//fun UnitConverter() {
+//
+//    Column {
+//
+//        Text("Unit Converter")
+//        OutlinedTextField(value = "", onValueChange = {})
+//        Row {
+//            // LoaclContext 안드로이드 액티비티(화면)를 가져온다.
+//            val context = LocalContext.current
+//
+//            // 버튼 클릭시 토스트 팝업으로 Thanks for clicking이 뜬다.
+//            Button(onClick = {
+//                Toast.makeText(context,
+//                    "Thanks for clicking!",
+//                    Toast.LENGTH_LONG).show()
+//            }) {
+//                // 버튼 메시지
+//                Text("Click Me")
+//            }
+//
+//        }
+//        Text("Result : ")
+//    }
+//}
+
+@Preview(showBackground = true)
+@Composable
+fun UnitConverterPreview() {
+    UnitConverter()
+}
+
+// AVD(Android Virtual Device) 가상시뮬까지 구동시킬 필요없이 코드를 프리뷰로 볼수있는 기능
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    MyFirstAppTheme {
+//        Greeting("HAHA")
+//    }
+//}
